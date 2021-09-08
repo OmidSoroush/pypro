4fe4a51ef5019c2caf1eae8fb6bbffa8fdba0046
Omid Soroush <61864572+OmidSoroush@users.noreply.github.com>
2021-09-08 12:32:08 +0200

    new look website

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 
-
-
 urlpatterns = [
     path('', views.HomePage.as_view(), name='blog-home'),
     path('about/', views.AboutPage.as_view(), name='blog-about'),
