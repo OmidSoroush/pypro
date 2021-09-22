579500dece256aeb1e1992b1bdee779dde939bc1
Omid Soroush <61864572+OmidSoroush@users.noreply.github.com>
2021-09-22 17:45:21 +0200

    final styling

--- pypro/settings.py
+++ pypro/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import json
+
+with open('/Users/omidsoroush/Desktop/config/config.json') as config_file:
+    config = json.load(config_file)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4fk4voj_f&5_5-=9nd^nje=x7a^%qi=6(fbi-ebl1e=cd@nvpq'
+SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
