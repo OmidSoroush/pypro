a4295c7fe4526ddfe3eccbf52a78b90b7ff10583
Omid Soroush <61864572+OmidSoroush@users.noreply.github.com>
2021-09-24 14:30:35 +0200

    added new redirects

--- users/views.py
+++ users/views.py
@@ -11,7 +11,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Your account has been created. You can log in now.')
-            return redirect('blog:blog-home')
+            return redirect('users:login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
