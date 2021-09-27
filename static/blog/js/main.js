13ab39fff6e89975083d2f46d4eb2ad7cd872e2f
Omid Soroush <61864572+OmidSoroush@users.noreply.github.com>
2021-09-27 14:33:35 +0200

    styling python_detail

--- static/blog/js/main.js
+++ static/blog/js/main.js
@@ -9,17 +9,17 @@
 
 
 // Hide Navbar on scroll down
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-80px";
-  }
-  prevScrollpos = currentScrollPos;
-
-};
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+// var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "-80px";
+//   }
+//   prevScrollpos = currentScrollPos;
+//
+// };
 
 (function() {
   "use strict";
