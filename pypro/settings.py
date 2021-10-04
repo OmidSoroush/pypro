b2db41661681c79c2560aaf98c1adcb23a92a498
Omid Soroush <61864572+OmidSoroush@users.noreply.github.com>
2021-10-04 22:04:26 +0200

    S3 integration

--- pypro/settings.py
+++ pypro/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'sendemail',
     'crispy_forms',
     'tinymce',
-    'tinymce_filebrowser',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -142,7 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 #STATICFILES_DIRS = [BASE_DIR / 'static',]
 
@@ -212,3 +212,12 @@ EMAIL_HOST_PASSWORD = config.get('EMAIL_PASS')
 # EMAIL_USE_SSL = True
 # EMAIL_HOST_USER = config.get('EMAIL_USER')
 # EMAIL_HOST_PASSWORD = config.get('EMAIL_PASS')
+
+
+AWS_ACCESS_KEY_ID = config.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
